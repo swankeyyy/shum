61c2b47659d757c4b675de661c00b8dae9de332c
levchuk <swankyyy1@gmail.com>
2024-10-04 15:30:53 +0300

    add database_helper with settings for sqlalchemy

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,5 @@
 from pydantic_settings import BaseSettings
-from pydantic import BaseModel
+from pydantic import BaseModel, PostgresDsn
 
 
 class RunConfig(BaseModel):
@@ -11,9 +11,17 @@ class ApiPrefix(BaseModel):
     prefix: str = "/api"
 
 
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
-    db_url: str
+    db: DatabaseConfig
     api_prefix: ApiPrefix = ApiPrefix()
 
 
---
+++ app/core/models/db_helper.py
@@ -0,0 +1,37 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine, async_sessionmaker, AsyncSession
+from core.config import settings
+
+
+class DatabaseHelper:
+    def __init__(self, url: str, poll_size: int = 5, echo: bool = False, echo_pool: bool = False,
+                 max_overflow: bool = 10) -> None:
+        self.engine: AsyncEngine = create_async_engine(
+            url=url,
+            echo=echo,
+            echo_pool=echo_pool,
+            pool_size=poll_size,
+            max_overflow=max_overflow,
+        )
+        self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
+            bind=self.engine,
+            autocommit=False,
+            autoflush=False,
+            expire_on_commit=False,
+        )
+
+    async def dispose(self):
+        """shut down the database engine"""
+        await self.engine.dispose()
+
+    async def session_getter(self):
+        async with self.session_factory() as session:
+            yield session
+
+
+db_helper = DatabaseHelper(
+    url=str(settings.db.url),
+    echo_pool=settings.db.echo_pool,
+    echo=settings.db.echo,
+    max_overflow=settings.db.max_overflow,
+    poll_size=settings.db.poll_size,
+)
